fda078e4ff615ebfbe9c5f1350ddb0f8707f858c
Ryan Rezaei <ryanrezaei1@gmail.com>
2026-06-10 22:11:00 -0700

    Restore Android shared client build

--- Android_Client/Open RSC Android Client/src/main/java/orsc/ScaledWindow.java
+++ Android_Client/Open RSC Android Client/src/main/java/orsc/ScaledWindow.java
@@ -22,4 +22,22 @@ public class ScaledWindow {
 	}
 	public void validateAppletSize() {
 	}
+	public static int getViewportPresetCount() {
+		return 1;
+	}
+	public static int getViewportPresetIndex() {
+		return 0;
+	}
+	public static void setViewportPresetIndex(int index) {
+	}
+	public static String getViewportPresetLabel() {
+		return "Phone";
+	}
+	public static String getViewportPresetLabel(int index) {
+		return getViewportPresetLabel();
+	}
+	public void applyViewportPreset(int index) {
+	}
+	public void applyStartupScalingDefaults(boolean hasSavedScalingScalar) {
+	}
 }
